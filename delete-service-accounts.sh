f92f40757f7dd9f3284823a0147764016f0e2eeb
Paolo Patierno <ppatierno@live.com>
2022-05-08 16:28:28 +0200

    Hacking service account and ACLs deletion
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- delete-service-accounts.sh
+++ delete-service-accounts.sh
@@ -2,11 +2,14 @@
 
 service_accounts=$(rhoas service-account list -o json)
 
+for client_id in $(echo "${service_accounts}" | jq -r .items[].client_id); do
+    echo "Deleting ACLs for service account client_id: $client_id"
+    rhoas kafka acl delete -y --pattern-type any --service-account $client_id
+done
+
 for id in $(echo "${service_accounts}" | jq -r .items[].id); do
-    echo "Deleting service account $id"
+    echo "Deleting service account id: $id"
     rhoas service-account delete -y --id $id
-    echo "Deleting corresponding ACLs"
-    rhoas kafka acl delete -y --pattern-type any --service-account $id
 done
 
 rm *.env
